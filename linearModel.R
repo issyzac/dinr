59c1bf147836ea28bfcc4c8843c9b18f8abeb8b4
issyzac <issyzac.iz@gmail.com>
2021-07-25 06:18:32 +0300

    Basics of linear model tutorial

---
+++ linearModel.R
@@ -0,0 +1,7 @@
+## Search the documentation of a function (or package or whatever)
+?lm
+
+# Create the dataset
+height <- c(176, 154, 138, 196, 132, 176, 181, 169, 150, 175)
+bodymass <- c(82, 49, 53, 112, 47, 69, 77, 71, 62, 78)
+
